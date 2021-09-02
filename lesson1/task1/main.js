001624bb61dd96aaf61415fe9776d0960fd738f5
Max <lasymacks@icloud.com>
2021-09-02 18:55:57 +0300

    final first task

--- lesson1/task1/main.js
+++ lesson1/task1/main.js
@@ -1,9 +1,7 @@
 const numbersConverter = () => {
     const num = prompt('Введите число');
     const system = prompt('Укажите систему исчисления');
-    if (isNaN(Number(num))) {
-        alert('Некорректный ввод');
-    } else if (isNaN(Number(system))) {
+    if (isNaN(Number(num)) || isNaN(Number(system))) {
         alert('Некорректный ввод');
     } else {
         console.log(Number(num).toString(system));
--- lesson1/task2/main.js
+++ lesson1/task2/main.js
@@ -1,9 +1,11 @@
 const primitiveCalculator = () => {
     const num1 = prompt('Введите первое число');
-    const num2 = prompt('Введите второе число');
     if (isNaN(Number(num1))) {
         alert('Некорректный ввод');
-    } else if (isNaN(Number(num2))) {
+        return;
+    }
+    const num2 = prompt('Введите второе число');
+    if (isNaN(Number(num2))) {
         alert('Некорректный ввод');
     } else {
         console.log(Number(num1) + Number(num2), num1 / num2);
