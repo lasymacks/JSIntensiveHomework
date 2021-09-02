021a25eeaea10b943b3b6195bc7d410b86130282
Max <lasymacks@icloud.com>
2021-09-02 07:00:35 +0300

    corrected first task

--- lesson1/task1/main.js
+++ lesson1/task1/main.js
@@ -1,5 +1,12 @@
-let numbersConverter = (num, system) => {
-    result = isNaN(Number(num)) ? alert('Некорректный ввод') : 
-    isNaN(Number(system)) ? alert('Некорректный ввод') : console.log(Number(num).toString(system));
+const numbersConverter = () => {
+    const num = prompt('Введите число');
+    const system = prompt('Укажите систему исчисления');
+    if (isNaN(Number(num))) {
+        alert('Некорректный ввод');
+    } else if (isNaN(Number(system))) {
+        alert('Некорректный ввод');
+    } else {
+        console.log(Number(num).toString(system));
+    }
 };
-numbersConverter(number = prompt('Введите число'), numberSystem = prompt('Укажите в какую систему исчисления конвертировать'));
+numbersConverter();
--- lesson1/task2/main.js
+++ lesson1/task2/main.js
@@ -1,5 +1,12 @@
-let primitiveCalculator = (num1, num2) => {
-    result = isNaN(Number(num1)) ? alert('Некорректный ввод') : 
-    isNaN(Number(num2)) ? alert('Некорректный ввод') : console.log(Number(num1) + Number(num2), num1 / num2);
+const primitiveCalculator = () => {
+    const num1 = prompt('Введите первое число');
+    const num2 = prompt('Введите второе число');
+    if (isNaN(Number(num1))) {
+        alert('Некорректный ввод');
+    } else if (isNaN(Number(num2))) {
+        alert('Некорректный ввод');
+    } else {
+        console.log(Number(num1) + Number(num2), num1 / num2);
+    }
 };
-primitiveCalculator(number1 = prompt('Введите первое число'), number2 = prompt('Введите второе число'));
+primitiveCalculator();
