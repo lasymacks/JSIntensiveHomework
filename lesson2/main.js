3d2a4ca742cbf8a5312c89c56e6b30cc4117f7c5
Max <lasymacks@icloud.com>
2021-09-06 20:10:48 +0300

    lesson2

---
+++ lesson2/main.js
@@ -0,0 +1,60 @@
+function makeObjectDeepCopy(obj) {
+    const copy = {};
+    for (let key in obj) {
+        if (typeof obj[key] === 'object') {
+            copy[key] = makeObjectDeepCopy(obj[key]);
+        } else {
+            copy[key] = obj[key];
+        }
+    }
+    return copy;
+}
+
+
+
+function selectFromInterval(NumbersArr, startNum, endNum) {
+    if (Array.isArray(NumbersArr)) {
+        NumbersArr.forEach(elem => {
+            if (typeof(elem) !== 'number') {
+                throw new Error('Ошибка!');
+            }
+        })
+        if (typeof(startNum) !== 'number' || typeof(endNum) !== 'number') {
+            throw new Error('Ошибка!');
+        }
+        if (startNum > endNum) {
+            [startNum, endNum] = [endNum, startNum];
+        }
+        return resultArr = NumbersArr.filter(item => item >= startNum && item <= endNum);
+    } else {
+        throw new Error('Ошибка!');
+    }
+}
+
+
+
+const myIterable = {from: 1, to: 4};
+
+myIterable[Symbol.iterator] = function() {
+    if (this.from === null || this.to === null) {
+        throw new Error('Ошибка!');
+    }
+    if (typeof(this.from) !== 'number' || typeof(this.to) !== 'number') {
+        throw new Error('Ошибка!');
+    } 
+    if (this.from > this.to) {
+        throw new Error('Ошибка!');
+    } else {
+        return {
+            current: this.from,
+            last: this.to,
+            next() {
+                if (this.current <= this.last) {
+                    return { done: false, value: this.current++ };
+                } else {
+                    return { done: true };
+                }
+            }
+        };
+    }
+};
